a41a0503d1c12f909b8682d1306a55bdad9bad5d
Kid-Chang <pch3310@gmail.com>
2023-03-11 11:47:05 +0900

    22/03/11 스프링 DB 접근 기술

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,21 +1,47 @@
 package hello.hellospring;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
 
 @Configuration
 public class SpringConfig {
-    @Bean
-    public MemberService memberService(){
-        return new MemberService(memberRepository());
+
+    // private DataSource dataSource;
+    //
+    // @Autowired
+    // public SpringConfig(DataSource dataSource){
+    //     this.dataSource = dataSource;
+    // }
+
+    // private EntityManager em;
+    //
+    // @Autowired
+    // public SpringConfig(EntityManager em) {
+    //     this.em = em;
+    // }
+
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
     @Bean
-    public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
+    public MemberService memberService(){
+        return new MemberService(memberRepository);
     }
+    // 기존에는 아래 memberRepository()를 통해 구현체를 호출해서 리턴했지만, 스프링 데이터 JPA를 이용할때는 구현체가 알아서 생성되므로, private final MemberRepository memberRepository; 하나 만들고 넣어주면 끝.
+
+    // @Bean
+    // public MemberRepository memberRepository(){
+    //     // return new MemoryMemberRepository();
+    //     // return new JdbcMemberRepository(dataSource);
+    //     // return new JdbcTemplateMemberRepository(dataSource);
+    //     return new JpaMemberRepository(em);
+    // }
 }
